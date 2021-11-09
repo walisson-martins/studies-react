7f89722f4224987de56a5de7ccb42be1f6eb114d
Walisson Martins da Silva <walisson.silva@implantainformatica.com.br>
2021-11-09 19:16:06 -0300

    pacotes, props, state, scss.

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,27 +1,13 @@
-import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Item from "./item";
 import style from "./Lista.module.scss";
 
-function Lista() {
-  const tarefa = [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
-    },
-    {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
-    },
-  ];
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
-        {tarefa.map((item, index) => (
+        {tarefas.map((item, index) => (
           <Item key={index} {...item} />
         ))}
       </ul>
--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -1,12 +1,19 @@
+import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
 export default function Item({
   tarefa,
   tempo,
-}: {
-  tarefa: string;
-  tempo: string;
-}) {
+  selecionado,
+  completado,
+  id,
+}: ITarefa) {
+  console.log('Item atual' +  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id, );
+  
   return (
     <li className={style.item}>
       <h3>{tarefa}</h3>
